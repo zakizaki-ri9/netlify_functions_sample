f189ae1bfedef46948446f16236ffa3fbc2e2678
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-10-20 13:47:38 +0900

    feat: message get logic add

--- src/event.js
+++ src/event.js
@@ -1,8 +1,30 @@
-import { WebClient } from '@slack/client'
+import {
+  WebClient
+} from '@slack/client'
 
 exports.handler = (event, context, callback) => {
+
+  // Slackから通知された情報を出力
   const body = JSON.parse(event.body)
-  console.log(JSON.stringify(body, null, 4))
+  console.log(JSON.stringify(body, null, 2))
+
+  if (
+    slackEvent &&
+    slackEvent.type === 'reaction_added' &&
+    slackEvent.item.type === 'message'
+  ) {
+    const web = new WebClient(process.env.SLACK_TOKEN)
+    const res = await web.conversations.history({
+      latest: slackEvent.item.ts,
+      limit: 1,
+      channel: slackEvent.item.channel,
+      inclusive: true
+    })
+    const message = res.message[0]
+
+    // slackclientで取得したメッセージを表示
+    console.log(JSON.stringify(message, null, 2))
+  }
 
   callback(null, {
     statusCode: 200,
