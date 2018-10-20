75096ac47fd6eda48292ac0f64008f1e564da11c
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-10-20 15:09:05 +0900

    feat: connpass search api exec not encode

--- src/connpass.js
+++ src/connpass.js
@@ -1,18 +1,39 @@
-exports.handler = function (event, context, callback) {
+import axios from 'axios'
 
-  console.log("start");
+exports.handler = async (event, context, callback) => {
 
-  let responce = {};
+  let body = event.body
+  console.log(JSON.stringify(body))
 
-  console.log(JSON.stringify(event, null, 2));
+  let result = null
+  if (body.event_id && body.event_id.length > 0) {
+    console.log("[PRD] searchExec")
+    result = await searchExec(body.event_id[0])
+  } else {
+    console.log("[TEST] searchExec")
+    result = await searchExec(103631)
+  }
+
+  console.log(result)
 
   callback(null, {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(responce)
-  });
+    body: JSON.stringify(result)
+  })
 
-  console.log("end");
+  async function searchExec(id) {
+    console.log("id: " + id.toString())
+
+    let connpassUrl = "https://connpass.com/api/v1/event/?event_id=" + id.toString()
+    console.log("url: " + connpassUrl)
+
+    // connpassイベントサーチAPIを実行
+    let res = await axios.get(connpassUrl)
+    console.log("res.data: " + JSON.stringify(res.data))
+
+    return res.data
+  }
 }
